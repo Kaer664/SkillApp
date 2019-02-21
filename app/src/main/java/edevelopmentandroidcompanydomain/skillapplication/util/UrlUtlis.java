dca0f0a0fba5054feda81f062acec2dd8f6de77d
fengyunuo <1441780164@qq.com>
2019-02-21 15:28:58 +0800

    添加工具类

---
+++ app/src/main/java/edevelopmentandroidcompanydomain/skillapplication/util/UrlUtlis.java
@@ -0,0 +1,17 @@
+package edevelopmentandroidcompanydomain.skillapplication.util;
+
+/**
+ * Created by 风雨诺 on 2019/1/15.
+ */
+
+public class UrlUtlis {
+    //查询红绿灯配置信息
+    public static final String GetTrafficLightInfo=
+            "https://party.jiqiaodashi.top/transportservice/type/jason/action/GetTrafficLightConfigAction.do";
+    //查询所有传感器的值
+    public static final String GET_ALL_SENSE=
+            "http://party.jiqiaodashi.top/transportservice/type/jason/action/GetAllSense.do";
+//    查询站台信息
+    public static final String GET_BUS_STATION_INFO=
+        "http://party.jiqiaodashi.top/transportservice/type/jason/action/GetBusStationInfo.do";
+}
